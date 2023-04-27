3324306957456944f810d9e958bf4266b03cdb7b
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-04-27 11:39:53 +0500

    upd6

--- src/store/recipeQuery.ts
+++ src/store/recipeQuery.ts
@@ -5,19 +5,19 @@ export const recipeQuery = createApi({
 	baseQuery: fetchBaseQuery({ baseUrl: `https://api.spoonacular.com/recipes/` }),
 	endpoints: (build) => ({
 		getRandom: build.query({
-			query: (num) => `random?apiKey=${import.meta.env.VITE_API_KEY}&number=${num}`
+			query: (num) => `random?apiKey=2389d97e9dad4363a7a43ae096c1ed08&number=${num}`
 		}),
 		getByCountryName: build.query({
-			query: (name) => `complexSearch?apiKey=${import.meta.env.VITE_API_KEY}&cuisine=${name}&number=20`
+			query: (name) => `complexSearch?apiKey=2389d97e9dad4363a7a43ae096c1ed08&cuisine=${name}&number=20`
 		}),
 		getBySearchValue: build.query({
-			query: (name) => `complexSearch?apiKey=${import.meta.env.VITE_API_KEY}&query=${name}&number=20`
+			query: (name) => `complexSearch?apiKey=2389d97e9dad4363a7a43ae096c1ed08&query=${name}&number=20`
 		}),
 		getById: build.query({
-			query: (id) => `${id}/information?apiKey=${import.meta.env.VITE_API_KEY}&number=1`
+			query: (id) => `${id}/information?apiKey=2389d97e9dad4363a7a43ae096c1ed08&number=1`
 		}),
 		getByDiet: build.query({
-			query: (name) => `complexSearch?apiKey=${import.meta.env.VITE_API_KEY}&diet=${name}&number=20`
+			query: (name) => `complexSearch?apiKey=2389d97e9dad4363a7a43ae096c1ed08&diet=${name}&number=20`
 		}),
 
 
